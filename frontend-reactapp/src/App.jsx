89a6e595a7b8288cc1f7d204de66d3f35a9fed35
Uday <2300033639@kluniversity.in>
2025-10-19 12:19:31 +0530

    aws docker git actions project

--- frontend-reactapp/src/App.jsx
+++ frontend-reactapp/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        {/* Your routes or other components can go here */}
+        <Navbar/>
       </BrowserRouter>
     </div>
   );
